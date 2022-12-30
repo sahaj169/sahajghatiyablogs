22e72a182fb3fa55435300baa9a5b7253fedc626
Sahaj Ghatiya <67413998+sahaj169@users.noreply.github.com>
2022-12-31 05:08:29 +0530

    loading added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,41 @@ import Layout from "../components/layout/layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Head from "next/head";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Loading from "../components/ui/loading/loading";
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      setLoading(true);
+    });
+    router.events.on("routeChangeComplete", () => {
+      setLoading(false);
+    });
+  }, [router]);
+
+  if (loading) {
+    return (
+      <Layout>
+        <Head>
+          <title>Sahaj Ghatiya Blogs</title>
+          <meta
+            name="description"
+            content="Sahaj Ghatiya Blog Website built on NextJs"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div style={{ paddingTop: "4.5rem", minHeight: "100vh" }}>
+          <Loading />
+        </div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
       <Head>
